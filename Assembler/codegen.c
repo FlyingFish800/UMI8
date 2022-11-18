70a4b0c5200576c566962aa43adfb79f2bd060df
Alex <aesymons888@gmail.com>
2022-11-18 00:27:36 -0800

    Added toInt that works for hex, binary, and octal

--- Assembler/codegen.c
+++ Assembler/codegen.c
@@ -2,23 +2,43 @@
 #include <stdlib.h>
 #include "structs.h"
 
-
+// Convert string to int regarudless of base
 int toInt(char *string){
     // Get bbase of nput
-    // Cut of 0x, 0b, 0o or whatever if necesarry
-    // return strtol
+    int base = 10;
+    
+    if (string[1] == 'x') base = 16;
+    if (string[1] == 'o') base = 8;
+    if (string[1] == 'b') base = 2;
 
-    return 0; //strtol();
+    // Cut of 0x, 0b, 0o or whatever if necesarry
+    int strStart = 0;
+    if (base != 10) strStart = 2;
+    char *trimmedStr = malloc(0);
+
+    while(string[strStart] != '\0'){
+        if (base == 10) {
+            // Base 10 is clean copy
+            trimmedStr = realloc(trimmedStr, strStart+1); 
+            trimmedStr[strStart] = string[strStart];
+        } else {
+            // Other bases need first two chars trimmed
+            trimmedStr = realloc(trimmedStr, strStart-1);
+            trimmedStr[strStart-2] = string[strStart];
+        }
+        strStart++;
+    }
+
+    // return strtol
+    return strtol(trimmedStr,NULL,base);
 }
 
 // Basic driver for testing
 int main(){
-    
-   char str[30] = "10";
-   long ret;
-
-   ret = strtol(str, NULL, 8);
-   printf("The number(unsigned long integer) is %ld\n", ret);
+   printf("The number(unsigned long integer) is %i\n", toInt("0b10"));
+   printf("The number(unsigned long integer) is %i\n", toInt("0x10"));
+   printf("The number(unsigned long integer) is %i\n", toInt("0o10"));
+   printf("The number(unsigned long integer) is %i\n", toInt("0010"));
 
    return 0;
 }
